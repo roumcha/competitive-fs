95a4a746d9a360112d66344bc4b426e4d00862df
Roumcha <68161142+roumcha@users.noreply.github.com>
2022-12-01 17:51:29 +0900

    メモ：ラムレーズンアイスがおいしかった

--- Samples/標準入力(stdin).fsx
+++ Samples/標準入力(stdin).fsx
@@ -1,33 +1,32 @@
-// 1行 -> 文字列
+// 行 -> string
 let s = stdin.ReadLine()
 
-// 1行 -> 整数
+// 行 -> int
 let n = stdin.ReadLine() |> int
 
-// 1行 -> 文字配列
+// 行 -> char[]
 let s2 = stdin.ReadLine().ToCharArray()
 
-// 1行 -> 空白区切り
+// 行 -> 空白区切り -> string[]
 let s3 = stdin.ReadLine().Split()
 
-// 1行　-> 空白区切り整数
-let a =
-    stdin.ReadLine().Split() |> Array.map int
+// 行　-> 空白区切り -> int[]
+let a = stdin.ReadLine().Split() |> Array.map int
 
-// 1行 -> 空白区切り -> 分けて代入 (1)
-// Pros: 短く書ける
-// Cons: すべて同じ型でのみ有効, パターンマッチ警告が出る
+// 行 -> 空白区切り -> 分けて代入
+// メリット: 短く書ける
+// デメリット: すべて同じ型限定、パターンマッチ警告が出る
 // 警告を消すには、ファイルの頭に #nowarn "25"
 #nowarn "25"
 let [| x; y |] = stdin.ReadLine().Split()
 
-// 1行 -> 空白区切り -> 分けて代入 (2)
-// Pros: 警告出ない, 個別に型変換できる(次項)
-// Cons: 長い, 書きづらい
+// 行 -> 空白区切り -> 分けて代入 (別解)
+// メリット: 警告出ない、個別に型変換できる(次項)
+// デメリット: 長くて書きづらい
 let x2, y2 =
     stdin.ReadLine().Split() |> fun a -> a.[0], a.[1]
 
-// 1行 -> 空白区切り -> 個別の型変換 -> 分けて代入
+// 行 -> 空白区切り -> バラバラの型 -> 分けて代入
 let x3, y3, z3 =
     stdin.ReadLine().Split()
     |> fun a -> int a.[0], float a.[1], a.[2]
@@ -36,7 +35,7 @@ let x3, y3, z3 =
 let jaggedAry =
     Array.init 10 (fun _ -> stdin.ReadLine().Split() |> Array.map int)
 
-// 10x(一定) の整数二次元配列
+// 10x(固定) の整数二次元配列
 let ary2d =
     Seq.init 10 (fun _ -> stdin.ReadLine().Split() |> Array.map int)
     |> array2D
